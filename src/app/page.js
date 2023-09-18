b12f5606bca5e69198c2daf34611248c4ae6ad4e
piicharpa <neuah2515@gmail.com>
2023-09-18 15:19:40 +0700

    .finished

--- src/app/page.js
+++ src/app/page.js
@@ -12,6 +12,7 @@ import {
   Title,
 } from "@mantine/core";
 import axios from "axios";
+import { set } from "lodash";
 import { useEffect, useState } from "react";
 
 export default function Home() {
@@ -33,17 +34,21 @@ export default function Home() {
     const resp = await axios.get("/api/course");
     setCourses(resp.data.courses);
     setLoadingCourses(false);
+    
   };
 
   const loadMyCourses = async () => {
+    setLoadingMyCourses(true)
     const resp = await axios.get("/api/enrollment", {
       headers: { Authorization: `Bearer ${token}` },
     });
     setMyCourses(resp.data.courses);
+    setLoadingMyCourses(false)
   };
 
   useEffect(() => {
     loadCourses();
+    
   }, []);
 
   useEffect(() => {
@@ -54,15 +59,19 @@ export default function Home() {
 
   const login = async () => {
     try {
+      setLoadingLogin(true);
       const resp = await axios.post("/api/user/login", { username, password });
       setToken(resp.data.token);
       setAuthenUsername(resp.data.username);
       setUsername("");
       setPassword("");
+      setLoadingLogin(false);
     } catch (error) {
+      setLoadingLogin(true);
       if (error.response.data) {
         alert(error.response.data.message);
       }
+      setLoadingLogin(false);
     }
   };
 
@@ -105,7 +114,10 @@ export default function Home() {
                 onChange={(e) => setPassword(e.target.value)}
                 value={password}
               />
-              <Button onClick={login}>Login</Button>
+              {!loadingLogin && (<Button onClick={login} >Login</Button>)}
+              {loadingLogin &&(<Button onClick={login} disabled >Login...</Button>)}
+              {/* {!loadingLogin ? (<Button onClick={login} >Login</Button>):(<Button onClick={login} disabled >Login...</Button>)} */}
+
             </Group>
           )}
           {authenUsername && (
@@ -133,9 +145,10 @@ export default function Home() {
             ))}
 
           {/* Do something with below loader!! */}
-          <Loader variant="dots" />
+          {loadingMyCourses && (
+          <Loader variant="dots" /> )}
         </Paper>
-        <Footer year="2023" fullName="Chayanin Suatap" studentId="650610560" />
+        <Footer year="2023" fullName="Picharpa Eamkanitchart" studentId="650610791" />
       </Stack>
     </Container>
   );
